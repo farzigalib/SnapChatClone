81afd49f87c22f37c9828a33e1b9da2b1fa67123
farzigalib <himanshjoshi3011@gmail.com>
2022-03-05 17:43:39 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+
+
+  return (
+    <View style={{ backgroundColor: '#fff', flex: 1 }}>
+      <Text style={{ color: '#000' }}>Hello World!</Text>
+    </View>
+  );
+};
+
+export default App;
